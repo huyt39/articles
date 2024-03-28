c32590e6efc7f2b3ead228e7a52ebabc0b90f272
Huy Tuan <hotuanhuy1203@gmail.com>
2024-03-28 22:09:23 +0700

    setup_graphql

--- index.ts
+++ index.ts
@@ -1,24 +1,54 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { connect as connectDatabase } from "./config/database";
+import {ApolloServer, gql} from "apollo-server-express";
 
 import Article from "./models/article.model";
+import { graphql } from "graphql";
+import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 
-dotenv.config();
-connectDatabase();
-const app: Express = express();
-const port: number | string = process.env.PORT || 3000;
 
-// Rest API
-app.get("/articles", async (req: Request, res: Response) => {
-  const articles = await Article.find({
-    deleted: false //lay ra bai viet chua bi xoa
-  })
-  res.json({
-    articles: articles //tra ra giao dien cac bai viet
+
+const startServer = async () => {
+  dotenv.config();
+  connectDatabase();
+  const app: Express = express();
+  const port: number | string = process.env.PORT || 3000;
+  
+  // GraphQL API
+  //dinh nghia ra truong ma FE duoc phep truy cap vao de lay data
+  // const typeDefs = gql`
+  //   type Query {
+  //     hello: String 
+  //   }
+  // `;
+  
+  // const resolvers = {
+  //   Query: {
+  //     hello: () => { //ham co nhiem vu tra ra 1 chuoi
+  //       return "Hello world";
+  //     }
+  //   }
+  
+  // }
+  
+  //khoi tao apollo sv:
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
   });
-});
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+  
+  await apolloServer.start(); //khoi chay apollo sv roi dinh nghia ra duong link api
+  
+  apolloServer.applyMiddleware({
+    app: app,           //app cua chung ta
+    path: "/graphql"  //duong link api
+  })
+  
+  
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+}
+startServer();
---
+++ resolvers.ts
@@ -0,0 +1,8 @@
+export const resolvers = {
+    Query: {
+      hello: () => { //ham co nhiem vu tra ra 1 chuoi
+        return "Hello world";
+      }
+    }
+  
+  }
