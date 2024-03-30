caf6dacea6ad401ef70a4dd5c7b8306fa7d7df84
Huy Tuan <hotuanhuy1203@gmail.com>
2024-03-30 15:51:11 +0700

    Them-sua bai viet kem categoryId

--- typeDefs.ts
+++ typeDefs.ts
@@ -24,7 +24,8 @@ export const typeDefs = gql`
   input ArticleInput {
     title: String,
     avatar: String,
-    description: String
+    description: String,
+    categoryId: String 
   }
 
   input CategoryInput{
